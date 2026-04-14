3aa64174255fe7b5ffa1a145eba08331a2e07592
RishSh123 <rishabhsharma.bl@gmail.com>
2026-04-14 19:38:42 +0530

    SkillsForm

--- client/src/components/ProjectForm.jsx
+++ client/src/components/ProjectForm.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Plus, Trash2 } from "lucide-react";
 
 const ProjectForm = ({ data, onChange }) => {
     const addProject = () => {
@@ -23,115 +24,76 @@ const ProjectForm = ({ data, onChange }) => {
   };
 
   return (
-    <div>
-      <div className="space-y-6">
+    <div >
       <div className="flex items-center justify-between">
         <div>
           <h3 className="flex items-center gap-2 text-lg font-semibold text-gray-900">
-            Education
+            Projects
           </h3>
-          <p className="text-sm text-gray-500">Add your educational background here.</p>
+          <p className="text-sm text-gray-500">Add your projects here.</p>
         </div>
 
         <button
-          onClick={addEducation}
+          onClick={addProject}
           className="flex items-center gap-2 px-3 py-1 text-sm bg-green-100 text-green-700 rounded hover:bg-green-200 transition-colors"
         >
           <Plus className="size-4" />
-          Add Education
+          Add Project
         </button>
       </div>
 
-      {data.length === 0 ? (
-        <div className="text-center py-8 text-gray-500">
-          <GraduationCap className="w-12 h-12 mx-auto mb-3 text-gray-300" />
-          <p>No education added yet.</p>
-          <p className="text-sm">Click "Add Education" to get started.</p>
-        </div>
-      ) : (
-        <div className="space-y-4">
-          {data.map((education, index) => (
+        <div className="space-y-4 mt-6">
+          {data.map((project, index) => (
             <div
               key={index}
               className="p-4 border border-gray-200  space-y-3"
             >
               <div className="flex justify-between items-start">
-                <h4>Education #{index + 1}</h4>
+                <h4>Project #{index + 1}</h4>
 
                 <button
-                  onClick={() => removeEducation(index)}
+                  onClick={() => removeProject(index)}
                   className="text-red-500 hover:text-red-700 transition-colors"
                 >
                   <Trash2 className="size-4" />
                 </button>
               </div>
 
-              <div className="grid md:grid-cols-2 gap-3">
+              <div className="grid gap-3">
                 <input
-                  value={education.institution || ""}
+                  value={project.name || ""}
                   onChange={(e) =>
-                    updateEducation(index, "institution", e.target.value)
+                    updateProject(index, "name", e.target.value)
                   }
                   type="text"
-                  placeholder="Institution Name"
-                  className="px-3 py-2 text-sm "
+                  placeholder="Project Name"
+                  className="px-3 py-2 text-sm rounded-lg"
                 />
 
                 <input
-                  value={education.degree || ""}
+                  value={project.type || ""}
                   onChange={(e) =>
-                    updateEducation(index, "degree", e.target.value)
+                    updateProject(index, "type", e.target.value)
                   }
                   type="text"
-                  placeholder="Degree (eg. Bachelor's, Master's)"
+                  placeholder="Project Type"
                   className="px-3 py-2 text-sm "
                 />
 
-                <input
-                  value={education.field || ""}
+                <textarea
+                rows={4}
+                  value={project.description || ""}
                   onChange={(e) =>
-                    updateEducation(index, "field", e.target.value)
+                    updateProject(index, "description", e.target.value)
                   }
-                  type="text"
-                  placeholder="Field of Study"
-                  className="px-3 py-2 text-sm "
-                />
-
-                <input
-                  value={education.graduation_date || ""}
-                  onChange={(e) =>
-                    updateEducation(index, "graduation_date", e.target.value)
-                  }
-                  type="month"
-                  className="px-3 py-2 text-sm "
-                />
-
-                <input
-                  value={education.gpa || ""}
-                  onChange={(e) =>
-                    updateEducation(index, "gpa", e.target.value)
-                  }
-                  type="text"
-                  placeholder="GPA"
-                  className="px-3 py-2 text-sm  disabled:bg-gray-100"
-                />
-
-                <input
-                  value={education.field || ""}
-                  onChange={(e) =>
-                    updateEducation(index, "field", e.target.value)
-                  }
-                  type="text"
-                  placeholder="Field of Study"
-                  className="px-3 py-2 text-sm "
+                  placeholder="Project Description"
+                  className="w-full px-3 py-2 text-sm rounded-lg resize-none  "
                 />
 
               </div>
             </div>
           ))}
         </div>
-      )}
-    </div>
     </div>
   )
 }
