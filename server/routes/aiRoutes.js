55d8c19efde33dac3a5844f43beaa061226e9a32
RishSh123 <rishabhsharma.bl@gmail.com>
2026-04-22 01:30:04 +0530

    Backend Complete

---
+++ server/routes/aiRoutes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { enhanceJobDescription, enhanceProfessionalSummary, uploadResume } from '../controllers/aiControllers.js';
+import protect from '../middlewares/authMiddleware.js';
+import { updateResume } from '../controllers/resumeController.js';
+
+
+
+const aiRouter = express.Router();
+
+aiRouter.post('/enhance-pro-sum', protect,enhanceProfessionalSummary);
+aiRouter.post('/enhance-job-desc', protect,enhanceJobDescription);
+aiRouter.post('/upload-resume',protect,uploadResume)
+
+export default aiRouter;
